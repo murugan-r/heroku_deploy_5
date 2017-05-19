31797a7e3b3f0e2b86b4790837fa337e35f81f51
Murugan <murugu2012@gmail.com>
2017-05-19 09:52:01 +0530

    Install Kaminari

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per(10)
     @location_hash = Gmaps4rails.build_markers(@users.where.not(:address_latitude => nil)) do |user, marker|
       marker.lat user.address_latitude
       marker.lng user.address_longitude
